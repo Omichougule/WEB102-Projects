72dc957996059d5567a77d1427b45b47cb532965
Omichougule <chougule.omkar10@gmail.com>
2025-06-09 01:39:35 -0400

    Added Header Component

---
+++ Project-1/food-truck/src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+
+const Header = () => (
+    <div className="flex flex-col items-center justify-center text-center">
+        <img src="/awning.png" alt="Awning" className="w-full max-w-5xl object-contain" />
+        <h1 className="text-5xl font-bold text-white mt-4 drop-shadow-lg">Food Truck Favorites</h1>
+    </div>
+
+);
+
+export default Header
