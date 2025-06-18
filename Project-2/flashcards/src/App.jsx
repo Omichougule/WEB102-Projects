32edeb619a292b9bf22762ec5479f5e7f09915f8
Omichougule <chougule.omkar10@gmail.com>
2025-06-17 23:12:24 -0400

    minor changes in App

--- Project-2/flashcards/src/App.jsx
+++ Project-2/flashcards/src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Card from './components/Card';
-import stringSimilarity from 'string-similarity';
 
 function App() {
   const [cards, setCards] = useState([
